279896fe2167a96a5f5f95e006bd27e890803a77
Dihan_Vermeulen <74895608+DihanVermeulen@users.noreply.github.com>
2022-08-17 08:50:53 +0200

    Updated stylesheet
    Changed body flex direction to column

--- src/javascript/UserList.js
+++ src/javascript/UserList.js
@@ -1,12 +1,27 @@
-let userDatabase = ["John", "Mary", "Alfred", "Brandon", "Peter", "Roxanne", "Chanelle", "Adrian", "Will", "Elle"];
-let selection = parseInt(prompt("Menu \n =============================== \n What would you like to do? \n 1) Add three users \n 2)Remove user \n 3)Display all users \n 4)Sort users"));
 let output = document.querySelector("#_output");
 
-switch(selection) {
-    case 1 : alert("You chose 1"); break;
-    case 3 : displayAllUsers(); break;
+function selectOption() {
+    let userDatabase = ["John", "Mary", "Alfred", "Brandon", "Peter", "Roxanne", "Chanelle", "Adrian", "Will", "Elle"];
+    let selection = parseInt(prompt("Menu \n =============================== \n What would you like to do? \n 1) Add three users \n 2)Remove user \n 3)Display all users \n 4)Sort users"));
+    switch (selection) {
+        case 1: for (x = 1; x <= 3; x++) {
+            let x = prompt("Type user name");
+            userDatabase.push(x.toString());
+        };
+            displayAllUsers(userDatabase);
+            break;
+        case 2: userDatabase.pop();
+            displayAllUsers(userDatabase);
+            break;
+        case 3: displayAllUsers(userDatabase);
+            break;
+        case 4: userDatabase.sort();
+            displayAllUsers(userDatabase);
+            break;
+        default: alert("Sorry, that option is unknown");
+    }
 }
 
-function displayAllUsers() {
-    output.innerHTML = userDatabase;
+function displayAllUsers(arr) {
+    output.innerHTML = arr;
 }
