62bddc12937220616344aaed5de5dcef3a8c319c
Dihan_Vermeulen <74895608+DihanVermeulen@users.noreply.github.com>
2022-08-17 08:26:31 +0200

    Updated javascript

--- src/javascript/BasicPokedex.js
+++ src/javascript/BasicPokedex.js
@@ -7,63 +7,75 @@ function logic() {
 
 
     // Will only give objects
-    console.log( name + " - " + type + " - " + region + " region") // remove once you have clear understanding
+    console.log(name + " - " + type + " - " + region + " region") // remove once you have clear understanding
 
 
     // Get String values of variables
     name = name.value;
     type = type.value;
     region = region.value;
-    
+
 
     // Can log them since they are now pure Strings
-    console.log( name + " - " + type + " - " + region + " region") // remove once you have clear understanding
+    console.log(name + " - " + type + " - " + region + " region") // remove once you have clear understanding
 
-/*-----------------------------------------------
- * Add your solution code here
- *---------------------------------------------*/
+    /*-----------------------------------------------
+     * Add your solution code here
+     *---------------------------------------------*/
 
     /* --- Part 1 : Create pokemon data Object --- */
 
-				// Use If to ensure input validation (ensure input fields are !empty)
-        // Create pokemon object here (const pokemon) using user input Strings
+    // Use If to ensure input validation (ensure input fields are !empty)
+    // Create pokemon object here (const pokemon) using user input Strings
 
 
-        // Create a span element (const pokemonData) and put the pokemon objects properties inside of it
+    // Create a span element (const pokemonData) and put the pokemon objects properties inside of it
+    let main = document.querySelector(".side");
+    if (name == "" || type == "" || region == "") {
+        alert("Please select or enter an option");
+    }
+    else {
+        const pokemon = { name: name, type: type, region: region };
+        let pokemonText = document.createTextNode(`name: ${pokemon.name},type: ${pokemon.type},region: ${pokemon.region}`);
+        let pokemonData = document.createElement("span");
+        pokemonData.appendChild(pokemonText);
+        main.appendChild(pokemonData);
 
+        console.log(pokemon);
 
+    }
 
     /* --- Part 2 : Creating other DOM Objects --- */
-        
-        // Create HTML li element here (const pokedexEntry)
+
+    // Create HTML li element here (const pokedexEntry)
+    const pokedexEntry = document.createElement("li");
+
+    // Give pokedexEntry (li) a new class equal to the "type" property of the pokemon object
 
 
-        // Give pokedexEntry (li) a new class equal to the "type" property of the pokemon object
-
-
-        // Creating a preview button (using a Template Literal)
-        // this code is for an html form that will Google search the name of the pokemon
-        const previewButton = `
+    // Creating a preview button (using a Template Literal)
+    // this code is for an html form that will Google search the name of the pokemon
+    const previewButton = `
             <form action="http://google.com/search" target="_blank">
                 <input name="q" hidden value="${name}">
                 <input type="submit">
             </form>
-        `
+        `;
 
-    
+
     /* --- Part 3 : Add things into the DOM --- */
 
-        // Firstly append pokemonData to the innerHTML of pokedexEntry
+    // Firstly append pokemonData to the innerHTML of pokedexEntry
 
 
-        // Secondly append the previewButton to innerHTML of pokedexEntry
+    // Secondly append the previewButton to innerHTML of pokedexEntry
 
 
-        // Lastly we will get the ul List element that is meant to contain all pokedex entries
-        // The we will append pokedexEntry to the innerHTML of the pokedexEntries ul element
+    // Lastly we will get the ul List element that is meant to contain all pokedex entries
+    // The we will append pokedexEntry to the innerHTML of the pokedexEntries ul element
 
 
-/*-----------------------------------------------
- * Solution must be above this comment
- *---------------------------------------------*/
+    /*-----------------------------------------------
+     * Solution must be above this comment
+     *---------------------------------------------*/
 }
