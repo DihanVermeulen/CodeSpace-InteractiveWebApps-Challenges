b2f7bbb3c29f29a3d3c09da777eef5b912279be6
Dihan_Vermeulen <74895608+DihanVermeulen@users.noreply.github.com>
2022-08-18 12:44:36 +0200

    Completed challenge

--- src/javascript/ClassesAndGetters.js
+++ src/javascript/ClassesAndGetters.js
@@ -1,15 +1,15 @@
- //Create a class called Tasks
-        //Make sure your class has a constructor with a private variable
-        //Within your Constructor, make sure you have a parameter of task which is equal to a private variable in your constructor
-        //Create a getter that returns the variable within your constructor
-        //Add your code below
+//Create a class called Tasks
+//Make sure your class has a constructor with a private variable
+//Within your Constructor, make sure you have a parameter of task which is equal to a private variable in your constructor
+//Create a getter that returns the variable within your constructor
+//Add your code below
 class Tasks {
     constructor(task) {
         this._task = task;
     }
-    get task() { 
+    get task() {
         return this._task;
-     }
+    }
 }
 //Instantiate your Task class and store the object in a variable
 //Console log your Tasks object, by calling the getter
@@ -18,5 +18,9 @@ class Tasks {
 //console.log(myObject.task)
 //Add your code below
 
-let x = new Tasks("do dishes");
-console.log(x.task);
+function formSubmit(event) {
+    let item = document.querySelector("#taskItem").value;
+    event.preventDefault();
+    let x = new Tasks(item);
+    console.log(x.task);
+}
